486748d97174860e6fd9943ec9479099ae90a469
edujeeho <edujeeho@gmail.com>
2024-01-31 15:49:08 +0900

    init

---
+++ src/main/java/com/example/jpanext/article/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.example.jpanext.article.entity;
+
+import com.example.jpanext.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment extends BaseEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+
+    private String content;
+    private String writer;
+
+    @ManyToOne
+    private Article article;
+}
