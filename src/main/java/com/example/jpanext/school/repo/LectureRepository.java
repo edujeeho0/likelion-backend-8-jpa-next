485fbd6385bdd86e590f6204f59d20b49232f7a2
edujeeho <edujeeho@gmail.com>
2024-02-01 14:31:29 +0900

    jpql Pagination

--- src/main/java/com/example/jpanext/school/repo/LectureRepository.java
+++ src/main/java/com/example/jpanext/school/repo/LectureRepository.java
@@ -1,10 +1,14 @@
 package com.example.jpanext.school.repo;
 
 import com.example.jpanext.school.entity.Lecture;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface LectureRepository
@@ -38,4 +42,35 @@ public interface LectureRepository
             @Param("start") Integer startTime,
             @Param("end") Integer endTime
     );
+
+    @Query(
+            value = "SELECT * FROM lecture " +
+                    "WHERE start_time = ?1 AND end_time = ?2 ",
+            nativeQuery = true
+    )
+    List<Lecture> findLecturesByTimeNative(
+            Integer startTime,
+            Integer endTime
+    );
+    /**
+     * <code>SELECT * FROM lecture WHERE day in ('mon', 'tue', 'wed')</code>
+     */
+    @Query("SELECT l FROM Lecture l WHERE l.day IN :days")
+    List<Lecture> findByDayIn(
+            @Param("days") Collection<String> days
+    );
+
+    // Pagination
+    @Query("SELECT l FROM Lecture l WHERE l.startTime < 12")
+    Page<Lecture> findLecturesBeforeLunch(Pageable pageable);
+
+    @Query("SELECT l FROM Lecture l WHERE l.startTime < 12")
+    List<Lecture> findLecturesBeforeLunch(Sort sort);
+
+    @Query(
+            value = "SELECT * FROM lecture WHERE start_time < 12",
+            countQuery = "SELECT COUNT(*) FROM lecture WHERE start_time < 12",
+            nativeQuery = true
+    )
+    Page<Lecture> findLecturesBeforeLunchNative(Pageable pageable);
 }
